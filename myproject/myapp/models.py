107c7b4549e76c8aaabbe269dc327272ed896a7a
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-23 20:51:27 -0500

    Se hace que el campo edad no sea obligatorio para probar la seguridad de Django

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    edad = models.PositiveIntegerField()
+    edad = models.PositiveIntegerField(null=True, blank=True)
     donador = models.BooleanField()
 
     def __str__(self):
