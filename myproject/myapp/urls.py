535bd9b8ef043509d25fe6a60e1353f92b8a1ce1
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-23 21:50:02 -0500

    Se agrega la direccion de lista personas a url py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('crear_persona/', views.crear_persona, name='crear_persona'),
+    path('lista_personas/', views.lista_personas, name='lista_personas'),
 ]
