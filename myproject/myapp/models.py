91e60018a89c599916d26685073bebc6b007dcb4
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-23 20:57:24 -0500

    Se restablecieron los cambios sobre el campo edad para continuar con el proyecto

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Persona(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
-    edad = models.PositiveIntegerField(null=True, blank=True)
+    edad = models.PositiveIntegerField()
     donador = models.BooleanField()
 
     def __str__(self):
